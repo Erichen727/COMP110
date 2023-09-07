f02714eb8e935599a73a04979903c1eee4dffd08
Eric Chen <Erichen@ad.unc.edu>
2023-09-07 14:25:04 -0400

    Progress

---
+++ exercises/Memory_diagram.py
@@ -0,0 +1,14 @@
+"""First  memory diagram"""
+
+""" name: str = "Eric"
+print ("Hello " + name)
+print ("Bye " + name) """
+
+
+
+"""Second Memory Diagram"""
+
+val: int = 1
+val2 : int = val + 3
+print (val + 1)
+print (val2)
---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,4 @@
+"""My first program for Comp110"""
+
+_author_ = "730705343"
+print ("Hello, world.")
